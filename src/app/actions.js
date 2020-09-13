1cb644d240ab39f8adacbbbbcb518e1af8db1b93
Adrian Piwowarczyk <adrpiw@gmail.com>
2020-09-13 13:04:29 +0100

    Moved authentication logic to actions

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,5 +1,25 @@
 import { AUTH_SUCCESS, AUTH_FAIL, AUTH_CLEAR } from './types';
 
+function authenticate(formData) {
+    return dispatch => {
+        fetch('http://localhost:8015/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
+            body: new URLSearchParams(formData),
+        })
+        .then(res => {
+            if (res.ok && res.status === 200) {
+                return dispatch(authSuccess());
+            }
+
+            return dispatch(authFail());
+        })
+        .catch(() => {
+            return dispatch(authFail());
+        })
+    }
+}
+
 const authSuccess = () => ({
     type: AUTH_SUCCESS
 });
@@ -16,4 +36,5 @@ export {
     authSuccess,
     authFail,
     authClear,
+    authenticate,
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,31 +3,18 @@ import { connect } from 'react-redux';
 import { Button, Grid, Header } from 'semantic-ui-react';
 
 import LoginForm from '../../components/LoginForm'
-import { authSuccess, authFail, authClear } from '../../app/actions';
+import { authClear, authenticate } from '../../app/actions';
 
-function Login({ dispatch, authStatus }) {
+function Login({ authenticate, authStatus, authClear }) {
+    
     function handleLogin(e) {
         const formData = new FormData(e.target);
-
-        fetch('http://localhost:8015/api/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
-            body: new URLSearchParams(formData),
-        })
-        .then(res => {
-            if (res.ok && res.status === 200) {
-                return dispatch(authSuccess());
-            }
-
-            return dispatch(authFail());
-        })
-        .catch(() => {
-            return dispatch(authFail());
-        })
+        
+        authenticate(formData);
     }
 
     function clearForm() {
-        dispatch(authClear());
+        authClear();
     }
 
     return (
@@ -53,4 +40,11 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(Login);
+const mapDispatchToProps = dispatch => {
+    return {
+        authenticate: (data) => dispatch(authenticate(data)),
+        authClear: () => dispatch(authClear())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
